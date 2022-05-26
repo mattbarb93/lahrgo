2dc74869c80e6bb85378ab5cd7428e067f3d08b3
Matheus Barbosa <mattbarb93@gmail.com>
2022-05-26 19:18:15 -0400

    dATA

--- src/components/paintings/data/ReadersData.js
+++ src/components/paintings/data/ReadersData.js
@@ -1,57 +1,21 @@
 export const ReadersData = [
   {
-    image: "../images/East Africa Rwandan Vegetables.jpg",
-    name: "Rwandan Vegetables",
-    type: "Gouache",
-    url: "http://localhost:3000/images/East Africa Rwandan Vegetables.jpg",
-  },
-  {
-    image: "../../images/Animals ceramic Yukon gouache.JPG",
-    name: "Ceramic Yukon Houache",
-    type: "Gouache",
-    url: "http://localhost:3000/images/Animals ceramic Yukon gouache.JPG",
-  },
-  {
-    image: "../images/East Africa Four Water Buffaloes.jpg",
-    name: "Water Buffalos",
-    type: "Oil",
-    url: "http://localhost:3000/images/East Africa Four Water Buffaloes.jpg",
-  },
-  {
-    image: "../images/East Africa Elephant Youth.jpg",
-    name: "Elephant Youth",
-    type: "Oil",
-    url: "http://localhost:3000/images/East Africa Elephant Youth.jpg",
-  },
-  {
-    image: "../images/East Africa Here Come the Elephants.jpg",
-    name: "Here Come The Elephants",
-    type: "Oil",
-    url: "http://localhost:3000/images/East Africa Here Come the Elephants.jpg",
-  },
-  {
-    image: "../images/East Africa Elephants Trooping by the Baobab Tree.jpg",
-    name: "Elephants Trooping by the Baobab Treet",
-    type: "Gouache",
-    url:
-      "http://localhost:3000/images/East Africa Elephants Trooping by the Baobab Tree.jpg",
-  },
-  {
-    image: "../images/East Africa Warthog.jpg",
-    name: "Warthog",
+    image: "../../images/Readers at Heckscher museum.jpg",
+    name: "Readers at the Heckscher Museum",
     type: "Pastel",
-    url: "http://localhost:3000/images/East Africa Warthog.jpg",
+    url: "http://localhost:3000/images/Readers at Heckscher museum.jpg",
   },
   {
-    image: "../images/East Africa Silverback.jpg",
-    name: "Silverback",
-    type: "Oil",
-    url: "http://localhost:3000/images/East Africa Silverback.jpg",
+    image: "../../images/Readers in Heckscher Park.jpg",
+    name: "Readers at the Heckscher Park",
+    type: "Pastel",
+    url: "http://localhost:3000/images/Readers in Heckscher Park.jpg",
   },
   {
-    image: "../images/East Africa Sleepy Baby Gorilla.jpg",
-    name: "Sleepy Baby Gorilla",
-    type: "Gouache",
-    url: "http://localhost:3000/images/East Africa Sleepy Baby Gorilla.jpg",
+    image: "../../images/Readers Mitchell reader with doggies.jpg",
+    name: "Reader with his Dogs",
+    type: "Pastel",
+    url:
+      "http://localhost:3000/images/Readers Mitchell reader with doggies.jpg",
   },
 ];
--- src/components/paintings/data/StillLifesData.js
+++ src/components/paintings/data/StillLifesData.js
@@ -1,57 +1,8 @@
 export const StillLifesData = [
   {
-    image: "../images/East Africa Rwandan Vegetables.jpg",
-    name: "Rwandan Vegetables",
-    type: "Gouache",
-    url: "http://localhost:3000/images/East Africa Rwandan Vegetables.jpg",
-  },
-  {
-    image: "../../images/Animals ceramic Yukon gouache.JPG",
-    name: "Ceramic Yukon Houache",
-    type: "Gouache",
-    url: "http://localhost:3000/images/Animals ceramic Yukon gouache.JPG",
-  },
-  {
-    image: "../images/East Africa Four Water Buffaloes.jpg",
-    name: "Water Buffalos",
-    type: "Oil",
-    url: "http://localhost:3000/images/East Africa Four Water Buffaloes.jpg",
-  },
-  {
-    image: "../images/East Africa Elephant Youth.jpg",
-    name: "Elephant Youth",
-    type: "Oil",
-    url: "http://localhost:3000/images/East Africa Elephant Youth.jpg",
-  },
-  {
-    image: "../images/East Africa Here Come the Elephants.jpg",
-    name: "Here Come The Elephants",
-    type: "Oil",
-    url: "http://localhost:3000/images/East Africa Here Come the Elephants.jpg",
-  },
-  {
-    image: "../images/East Africa Elephants Trooping by the Baobab Tree.jpg",
-    name: "Elephants Trooping by the Baobab Treet",
-    type: "Gouache",
-    url:
-      "http://localhost:3000/images/East Africa Elephants Trooping by the Baobab Tree.jpg",
-  },
-  {
-    image: "../images/East Africa Warthog.jpg",
-    name: "Warthog",
-    type: "Pastel",
-    url: "http://localhost:3000/images/East Africa Warthog.jpg",
-  },
-  {
-    image: "../images/East Africa Silverback.jpg",
-    name: "Silverback",
-    type: "Oil",
-    url: "http://localhost:3000/images/East Africa Silverback.jpg",
-  },
-  {
-    image: "../images/East Africa Sleepy Baby Gorilla.jpg",
-    name: "Sleepy Baby Gorilla",
-    type: "Gouache",
-    url: "http://localhost:3000/images/East Africa Sleepy Baby Gorilla.jpg",
+    image: "../../images/Still Life Sunflower Bouquet.jpg",
+    name: "Sunflower Bouquet",
+    type: "Oil Pastel",
+    url: "http://localhost:3000/images/Still Life Sunflower Bouquet.jpg",
   },
 ];
