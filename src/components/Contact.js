c56cd1aa4b37fcb3f72bbf8499a4f7b7ee83d67e
Matheus Barbosa <mattbarb93@gmail.com>
2022-07-07 14:29:55 -0400

    Input validation and confirmation message on Contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,37 +9,39 @@ const Contact = (e) => {
 
     emailjs.sendForm('service_jdsqiyg', 'template_tawmsjs', form.current, 'pox8a-PJjKSpbDgy6')
       .then((result) => {
-          console.log(result.text);
+        console.log(result.text);
       }, (error) => {
-          console.log(error.text);
+        console.log(error.text);
       });
     e.target.reset();
+    alert('Your message has been sent!')
   }
-    
 
-  
+
+
+
   return <div>
     <h1>CONTACT</h1>
 
-      <div>
-        <form ref={form} onSubmit={sendEmail} className="ui inverted form">
-          <div className="field">
-            <label>Name</label>
-            <input type="text" name="name" placeholder="Name"/>
-          </div>
-          <div className="field">
-            <label>Email</label>
-            <input type="email" name="email" placeholder="Email" />
-          </div>
-          <div className="field">
-            <label>Message</label>
-            <textarea name="message" type="text" rows="4" placeholder="Message" />
-          </div>
-          <button className="ui red button" type="submit">Submit</button>
-        </form>
-      </div>
+    <div>
+      <form ref={form} onSubmit={sendEmail} className="ui inverted form">
+        <div className="field">
+          <label>Name</label>
+          <input type="text" name="name" placeholder="Name" required />
+        </div>
+        <div className="field">
+          <label>Email</label>
+          <input type="email" name="email" placeholder="Email" required />
+        </div>
+        <div className="field">
+          <label>Message</label>
+          <textarea name="message" type="text" rows="4" placeholder="Message" required />
+        </div>
+        <button className="ui red button" type="submit">Submit</button>
+      </form>
     </div>
-  
+  </div>
+
 };
 
 export default Contact;
